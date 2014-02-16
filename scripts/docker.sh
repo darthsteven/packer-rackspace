4bd857a82ba5fffbfa51175e3ec9b7d61aa7684c
Don Ky <dkynyc@gmail.com>
2014-02-16 14:27:05 -0500

    docker.sh now non-interactive, bootstrap

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -7,8 +7,8 @@ $APT_CMD update
 $APT_CMD install apt-transport-https
 $APT_CMD install curl
 
-#docker 
+#Bootstrap Docker 
 curl http://get.docker.io/gpg | apt-key add -
 echo deb $DOCKER_REPO > /etc/apt/sources.list.d/docker.list
 $APT_CMD update
-$APT_CMD install --force-yes lxc-docker
+DEBIAN_FRONTEND=noninteractive apt-get install -q -y lxc-docker
